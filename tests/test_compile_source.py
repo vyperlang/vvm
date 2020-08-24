f1c04662dfc3d1be8d92b86f6db6326ce6c7d1d3
Ben Hauser <ben@hauser.id>
2020-08-24 20:18:51 +0300

    test: to_vyper_version

--- tests/test_compile_source.py
+++ tests/test_compile_source.py
@@ -1,3 +1,5 @@
+import pytest
+
 import vvm
 
 
@@ -16,3 +18,15 @@ def test_compile_standard(input_json, foo_source):
     result = vvm.compile_standard(input_json)
 
     assert "contracts/Foo.vy" in result["contracts"]
+
+
+@pytest.mark.parametrize("version_str", ["0.1.0b16", "0.1.0beta17"])
+def test_pragmas_in_vyper_010(version_str):
+    source = f"""
+# @version {version_str}
+
+@public
+def foo() -> int128:
+    return 42
+    """
+    vvm.compile_source(source, vyper_version=version_str)
---
+++ tests/test_to_vyper_version.py
@@ -0,0 +1,12 @@
+import pytest
+from semantic_version import Version
+
+from vvm.utils.convert import to_vyper_version
+
+
+@pytest.mark.parametrize(
+    "version_str",
+    ["0.1.0b17", "0.1.0beta17", "0.1.0-beta17", "0.1.0.beta17", "0.1.0B17", "0.1.0.Beta.17"],
+)
+def test_to_vyper_version(version_str):
+    assert to_vyper_version(version_str) == Version("0.1.0-beta.17")
