68a8fd3952a69e1329a7ff7cd929a99b6af32d90
Peter Lemenkov <lemenkov@gmail.com>
2022-10-31 22:21:33 +0100

    Switch to built-in setuptools functionality
    Looks like setuptools-markdown is deprecated in favor of a built-in
    setuptools functionality. Worse is that setuptools_markdown isn't
    compatible with a recent versions of a pypandoc library. Let's just drop
    it.
    
    See also  man-group/pytest-plugins#87.
    
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@
 
 from setuptools import find_packages, setup
 
+with open('README.md', encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="vvm",
     version="0.1.0",  # don't change this manually, use bumpversion instead
     description="Vyper version management tool",
-    long_description_markdown_filename="README.md",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Ben Hauser",
     author_email="ben@hauser.id",
     url="https://github.com/vyperlang/vvm",
     include_package_data=True,
     py_modules=["vvm"],
-    setup_requires=["setuptools-markdown"],
     python_requires=">=3.6, <4",
     install_requires=["requests>=2.19.0,<3", "semantic_version>=2.8.1,<3"],
     license="MIT",
