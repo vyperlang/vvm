1dc2eddf554ec71af797db6be3e66c5781204d36
Charles Cooper <cooper.charles.m@gmail.com>
2024-09-17 08:08:41 -0700

    Update tests/test_versioning.py

--- tests/test_versioning.py
+++ tests/test_versioning.py
@@ -57,5 +57,6 @@ def test_version_does_not_exist():
 def test_npm_version_for_04_release():
     with pytest.raises(UnexpectedVersionError) as excinfo:
         detect_vyper_version_from_source("# pragma version ^0.4.0")
+
     expected_msg = "Please use the pypi-style version specifier for vyper versions >= 0.4.0"
     assert str(excinfo.value) == msg
