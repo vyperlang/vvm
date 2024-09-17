f08333c046a3e7e64f1bc34bc50775ec374c4ec9
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-09-17 17:37:19 +0200

    Reintroduce variables

--- tests/test_versioning.py
+++ tests/test_versioning.py
@@ -58,6 +58,7 @@ def test_npm_version_for_04_release():
     with pytest.raises(UnexpectedVersionError) as excinfo:
         detect_vyper_version_from_source("# pragma version ^0.4.1")
 
-    assert str(excinfo.value) == (
+    expected_msg = (
         "Please use the pypi-style version specifier for vyper versions >= 0.4.0 (hint: ~=0.4.1)"
     )
+    assert str(excinfo.value) == expected_msg
