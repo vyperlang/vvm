13882db2c74b14d8fe6df771801bc3fead437d1e
trocher <43437004+trocher@users.noreply.github.com>
2024-11-19 11:33:28 +0100

    better comment

--- vvm/main.py
+++ vvm/main.py
@@ -76,9 +76,8 @@ def compile_source(
         )
 
     if output_format in ("combined_json", None):
-        # If someone was to compile a file named `version` (without extension),
-        # the json would contain a single entry that we would pop here.
-        # it is assumed that developers are not compiling files named `version`.
+        # Pop the version as it might be the first key in the dictionary
+        # We assumed the source file is not named `version` (without extension)
         compiler_data.pop("version", None)
         return {"<stdin>": list(compiler_data.values())[0]}
     return compiler_data
